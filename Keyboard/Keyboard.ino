e87acad02d769153b28598962a41372b168c5d60
Dominik <dkonik@umich.edu>
2016-12-23 15:47:02 -0500

    Got base version done w/ practice script

--- Keyboard/Keyboard.ino
+++ Keyboard/Keyboard.ino
@@ -1,6 +1,7 @@
 // TODO:
 // 1) Fix that shift,ctrl, and other asyncronous presses aren't always
 //    sent when pressed down
+// 2) Make it so that immediately after removing fingers from combo, key can't be pressed
 
 // Amount of time to wait between sending keys when key is being held down
 // in micros
@@ -10,7 +11,7 @@ const unsigned long PAUSE_AFTER_PRESS = 400000;
 // The amount of time to wait until sending character from initial
 // press. Don't want this to be too quick in case there is a combo
 // which is coming up
-const unsigned long INITIAL_HOLDOFF = 25000;
+const unsigned long INITIAL_HOLDOFF = 35000;
 const unsigned NUM_FINGERS = 10;
 
 bool first_time = false;
@@ -20,44 +21,50 @@ unsigned long time;
 // CHARACTER COMBO DEFINITIONS **********************************
 const uint16_t CTRL = 0b0000000001;
 const uint16_t SHIFT = 0b1000000000;
-const uint16_t ESCAPE = 0b0011101100;
+const uint16_t ENTER = 0b0001100000;
+const uint16_t UP = 0b0001101000;
+const uint16_t DOWN = 0b0010100100;
 const uint16_t LESS_THAN = 0b0000100100;
 const uint16_t BACK_SPACE = 0b0000000010;
 const uint16_t COMMA = 0b0000100100;
 const uint16_t TAB = 0b0100000000;
+const uint16_t LEFT = 0b0011100000;
 const uint16_t FUNCTION = 0b1000010001;
 const uint16_t SPACE = 0b0010000000;
 const uint16_t PERIOD = 0b0000101000;
-const uint16_t ENTER = 0b0000000100;
+const uint16_t RIGHT = 0b0000101100;
+const uint16_t ESCAPE = 0b0011101100;
 const uint16_t SPECIAL_ACTIVATOR = 0b0000100000;
-const uint16_t a = 0b0000001100;
-const uint16_t c = 0b0001000010;
-const uint16_t b = 0b0010001100;
-const uint16_t e = 0b0000000010;
-const uint16_t d = 0b0000001010;
-const uint16_t g = 0b0001001100;
-const uint16_t f = 0b0100000100;
-const uint16_t i = 0b0011000000;
-const uint16_t h = 0b0100000010;
-const uint16_t k = 0b0100001100;
-const uint16_t j = 0b0110001000;
-const uint16_t m = 0b0100001000;
-const uint16_t l = 0b0001000100;
-const uint16_t o = 0b0000000110;
-const uint16_t n = 0b0110000000;
-const uint16_t q = 0b0110000100;
-const uint16_t p = 0b0011000100;
-const uint16_t s = 0b0010000100;
-const uint16_t r = 0b0101000000;
-const uint16_t u = 0b0010001000;
-const uint16_t t = 0b0001001000;
-const uint16_t w = 0b0010000010;
-const uint16_t v = 0b0011000010;
-const uint16_t y = 0b0011001000;
-const uint16_t x = 0b0001000110;
+const uint16_t a = 0b0000000110;
+const uint16_t c = 0b0010001000;
+const uint16_t b = 0b0011000010;
+const uint16_t e = 0b0001001000;
+const uint16_t d = 0b0001000100;
+const uint16_t g = 0b0011001000;
+const uint16_t f = 0b0001001100;
+const uint16_t i = 0b0110000000;
+const uint16_t h = 0b0101000000;
+const uint16_t k = 0b0110001000;
+const uint16_t j = 0b0001000110;
+const uint16_t m = 0b0010000010;
+const uint16_t l = 0b0001000010;
+const uint16_t o = 0b0011000000;
+const uint16_t n = 0b0010000100;
+const uint16_t q = 0b0010000110;
+const uint16_t p = 0b0010001100;
+const uint16_t s = 0b0100000010;
+const uint16_t r = 0b0000001010;
+const uint16_t u = 0b0100001000;
+const uint16_t t = 0b0000001100;
+const uint16_t w = 0b0100000100;
+const uint16_t v = 0b0100001100;
+const uint16_t y = 0b0011000100;
+const uint16_t x = 0b0110000100;
 const uint16_t ALT = 0b0000010000;
-const uint16_t z = 0b0010000110;
+const uint16_t z = 0b0101001000;
 const uint16_t GRTR_THAN = 0b1000101000;
+
+
 // END CHARACTER COMBO DEFINITIONS ******************************
 
 // CHARACTER BUFFER VALUES **************************************
@@ -70,7 +77,36 @@ const uint8_t ENTER_VAL = 0x28;
 const uint8_t BACK_SPACE_VAL = 0x2A;
 const uint8_t SPACE_VAL = 0x2C;
 const uint8_t TAB_VAL = 0x2B;
-
+const uint8_t a_VAL = 0x4;
+const uint8_t b_VAL = 0x5;
+const uint8_t c_VAL = 0x6;
+const uint8_t d_VAL = 0x7;
+const uint8_t e_VAL = 0x8;
+const uint8_t f_VAL = 0x9;
+const uint8_t g_VAL = 0xa;
+const uint8_t h_VAL = 0xb;
+const uint8_t i_VAL = 0xc;
+const uint8_t j_VAL = 0xd;
+const uint8_t k_VAL = 0xe;
+const uint8_t l_VAL = 0xf;
+const uint8_t m_VAL = 0x10;
+const uint8_t n_VAL = 0x11;
+const uint8_t o_VAL = 0x12;
+const uint8_t p_VAL = 0x13;
+const uint8_t q_VAL = 0x14;
+const uint8_t r_VAL = 0x15;
+const uint8_t s_VAL = 0x16;
+const uint8_t t_VAL = 0x17;
+const uint8_t u_VAL = 0x18;
+const uint8_t v_VAL = 0x19;
+const uint8_t w_VAL = 0x1a;
+const uint8_t x_VAL = 0x1b;
+const uint8_t y_VAL = 0x1c;
+const uint8_t z_VAL = 0x1d;
+const uint8_t UP_VAL = 0x52;
+const uint8_t DOWN_VAL = 0x51;
+const uint8_t LEFT_VAL = 0x50;
+const uint8_t RIGHT_VAL = 0x4F;
 // END CHARACTER BUFFER VALUES **********************************
 
 uint8_t buf[8] = { 0 }; /* Keyboard report buffer */
@@ -164,24 +200,117 @@ void send_key(){
       buf[index++] = ALT_VAL;
     }
   }
-  // Special characters (special activator not engaged)
-  if((current_key & SPECIAL_ACTIVATOR) == SPECIAL_ACTIVATOR){
-    if(current_key == SPECIAL_ACTIVATOR){
-      return;
-    }
+  //Remove the ctrl, shift, and alt (or function) characters
+  uint16_t current_key_removed = current_key & 0b0111101110;
+  if(current_key_removed == a){
+    buf[index++] = a_VAL;
   }
-  // "Regular" characters
-  else{
-    if(current_key == 0b0001000000){
-      buf[2] = 0b00010101;
-      Serial.write(buf, 8);
-    }
-    if(current_key == 0b0001001000){
-      buf[2] = 0xE0;
-      buf[3] = 0x19;
-      Serial.write(buf, 8);
-    }
+  else if (current_key_removed == b){
+    buf[index++] = b_VAL;
   }
+  else if(current_key_removed == c){
+    buf[index++] = c_VAL;
+  }
+  else if(current_key_removed == d){
+    buf[index++] = d_VAL;
+  }
+  else if(current_key_removed == e){
+    buf[index++] = e_VAL;
+  }
+  else if(current_key_removed == f){
+    buf[index++] = f_VAL;
+  }
+  else if(current_key_removed == g){
+    buf[index++] = g_VAL;
+  }
+  else if(current_key_removed == h){
+    buf[index++] = h_VAL;
+  }
+  else if(current_key_removed == i){
+    buf[index++] = i_VAL;
+  }
+  else if(current_key_removed == j){
+    buf[index++] = j_VAL;
+  }
+  else if(current_key_removed == k){
+    buf[index++] = k_VAL;
+  }
+  else if(current_key_removed == l){
+    buf[index++] = l_VAL;
+  }
+  else if(current_key_removed == m){
+    buf[index++] = m_VAL;
+  }
+  else if(current_key_removed == n){
+    buf[index++] = n_VAL;
+  }
+  else if(current_key_removed == o){
+    buf[index++] = o_VAL;
+  }
+  else if(current_key_removed == p){
+    buf[index++] = p_VAL;
+  }
+  else if(current_key_removed == q){
+    buf[index++] = q_VAL;
+  }
+  else if(current_key_removed == r){
+    buf[index++] = r_VAL;
+  }
+  else if(current_key_removed == s){
+    buf[index++] = s_VAL;
+  }
+  else if(current_key_removed == t){
+    buf[index++] = t_VAL;
+  }
+  else if(current_key_removed == u){
+    buf[index++] = u_VAL;
+  }
+  else if(current_key_removed == v){
+    buf[index++] = v_VAL;
+  }
+  else if(current_key_removed == w){
+    buf[index++] = w_VAL;
+  }
+  else if(current_key_removed == x){
+    buf[index++] = x_VAL;
+  }
+  else if(current_key_removed == y){
+    buf[index++] = y_VAL;
+  }
+  else if(current_key_removed == z){
+    buf[index++] = z_VAL;
+  } 
+  else if(current_key_removed == TAB){
+    buf[index++] = TAB_VAL;
+  } 
+  else if(current_key_removed == SPACE){
+    buf[index++] = SPACE_VAL;
+  }
+  else if(current_key_removed == ENTER){
+    buf[index++] = ENTER_VAL;
+  }
+  else if(current_key_removed == BACK_SPACE){
+    buf[index++] = BACK_SPACE_VAL;
+  }
+  else if(current_key_removed == PERIOD){
+    buf[index++] = PERIOD_VAL;
+  }
+  else if(current_key_removed == COMMA){
+    buf[index++] = COMMA_VAL;
+  }
+  else if(current_key_removed == DOWN){
+    buf[index++] = DOWN_VAL;
+  }
+  else if(current_key_removed == LEFT){
+    buf[index++] = LEFT_VAL;
+  }
+  else if(current_key_removed == UP){
+    buf[index++] = UP_VAL;
+  }
+  else if(current_key_removed == RIGHT){
+    buf[index++] = RIGHT_VAL;
+  }
+  Serial.write(buf, 8);
 }
 
 // Makes computer stop writing character, should be called
---
+++ PracticeScript.py
@@ -0,0 +1,72 @@
+import sys
+
+chars = {}
+
+#Frequently used
+chars["SHIFT"] = 				"0b1000000000"	
+chars["\t"] = 					"0b0100000000"
+chars[" "] = 					"0b0010000000"
+chars["\n"] = 					"0b0001000000"
+chars["SPECIAL_ACTIVATOR"] = 	"0b0000100000"
+chars["ALT"] = 					"0b0000010000"
+# Left and right click???
+chars["BACK_SPACE"] = 			"0b0000000010"
+chars["CTRL"] = 				"0b0000000001"
+
+
+#Regular letters
+chars["e"] = 					"0b0001001000"
+chars["t"] =					"0b0000001100"
+chars["a"] = 					"0b0000000110"
+chars["o"] = 					"0b0011000000"
+chars["i"] =					"0b0110000000"
+chars["n"] = 					"0b0010000100"
+chars["s"] = 					"0b0100000010"
+chars["h"] = 					"0b0101000000"
+chars["r"] = 					"0b0000001010"
+chars["d"] = 					"0b0001000100"
+chars["l"] = 					"0b0001000010"
+chars["c"] = 					"0b0010001000"
+chars["u"] = 					"0b0100001000"
+chars["m"] =					"0b0010000010"
+chars["w"] = 					"0b0100000100"
+chars["f"] = 					"0b0001001100"
+chars["g"] = 					"0b0011001000"
+chars["y"] = 					"0b0011000100"
+chars["p"] = 					"0b0010001100"
+chars["b"] = 					"0b0011000010"
+chars["v"] =					"0b0100001100"
+chars["k"] =					"0b0110001000"
+chars["j"] = 					"0b0001000110"
+chars["x"] = 					"0b0110000100"
+chars["q"] = 					"0b0010000110"
+chars["z"] =					"0b0101001000"
+# Special case, shift+alt+ctrl == function button, maybe thing of better
+# way to do this
+
+# Special Characters (must be used with left thumb down)
+chars["."] = 					"0b0000101000"
+chars[","] = 					"0b0000100100"
+chars[">"] = 					"0b1000101000"
+chars["<"] = 					"0b0000100100"
+
+
+def main():
+	input_string = sys.argv[1]
+	for word in input_string.split():
+		word_func(word)
+
+
+def word_func(word):
+	print word
+	for character in word:
+		character = character.lower()
+		if character in chars:
+			print character + ": " + chars[character][2:6] + " " + chars[character][8:12]
+	word_in = raw_input()
+	if word_in != word:
+		print "Incorrect. Try again."
+		word_func(word)
+
+if __name__ == '__main__':
+	main()
