117622cbfc870f2703315ad85ba0c4fe4058d179
Dominik <dkonik@umich.edu>
2017-01-15 21:57:43 -0500

    Began interhand communication development

---
+++ RightHandCommunication/RightHandCommunication.ino
@@ -0,0 +1,16 @@
+// Using Leonardo
+
+int CE = 13;
+int CSN = 12;
+int IRQ = 2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
