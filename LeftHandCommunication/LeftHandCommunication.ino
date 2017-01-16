74f315161127d894321e42e71632e2bcea24f151
Dominik <dkonik@umich.edu>
2017-01-15 23:32:39 -0500

    Basic communcation working (no irq)

--- LeftHandCommunication/LeftHandCommunication.ino
+++ LeftHandCommunication/LeftHandCommunication.ino
@@ -1,14 +1,29 @@
 // Using micro
-int CE = 13;
-int CSN = 12;
-int IRQ = 2;
+#include  <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
 
-void setup() {
-  // put your setup code here, to run once:
+const int CE = 13;
+const int CSN = 12;
+const int IRQ = 2;
+char msg[2] = "hi";
 
+// Args = [ce_pin, csn_pin]
+RF24 radio(CE, CSN);
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int button = 7;
+
+void setup(void){
+ Serial.begin(9600);
+ radio.begin();
+ radio.openWritingPipe(pipe);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+ 
+void loop(void){
+ if (digitalRead(button) == HIGH){
+   Serial.println("Sending shit");
+   radio.write(msg, 2);
+ }
 }
--- RightHandCommunication/RightHandCommunication.ino
+++ RightHandCommunication/RightHandCommunication.ino
@@ -1,16 +1,51 @@
 // Using Leonardo
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
 
-int CE = 13;
-int CSN = 12;
-int IRQ = 2;
+const int CE = 13;
+const int CSN = 12;
+const int IRQ = 2;
 
+char msg[2];
 
-void setup() {
-  // put your setup code here, to run once:
+// Args = [ce_pin, csn_pin]
+RF24 radio(CE, CSN);
 
+//void check_radio(){
+//  Serial.println("Hi");
+//  bool tx, fail, rx;
+//  radio.whatHappened(tx, fail, rx);
+//
+//  // Will ignore fail and tx because we will never send
+//  // from this hand
+//
+//  // Message was received
+//  if(rx){
+//    radio.read(&message, sizeof(message));
+//    Serial.println(message);
+//  }
+//}
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+void setup(void) {
+  Serial.begin(9600);
+  radio.begin();
+
+  // args = [pipe#, pipe_address]
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  // attachInterrupt(digitalPinToInterrupt(IRQ), check_radio, FALLING);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+//Do nothing because check_radio handles everything
+void loop(void) {
+  if (radio.available()) {
+    radio.read(msg, 2);
+    Serial.println(msg);
+  } else {
+    Serial.println("No radio avail.");
+  }
+  delay(100);
 }
